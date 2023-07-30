489d1fc4af2174f72f860975a64fc225a0314118
Richard Narh <pascalcppjava@gmail.com>
2023-07-30 19:23:14 +0000

    Updates on prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  baseApi: 'api/v1',
+  endpoint: 'http://185.218.125.78:8080/invoicemaster/api/v1',
+  lookupEndpoint: 'http://185.218.125.78:8080/invoicemaster/api/v1/data'
 };
