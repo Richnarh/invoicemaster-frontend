a4df071b13dc0e2c8ea39edbf664f3853ff8bbb9
Richard Narh <pascalcppjava@gmail.com>
2023-09-27 23:11:55 +0000

    updates on adding https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   baseApi: 'api/v1',
-  endpoint: 'http://185.218.125.78:8080/invoicemaster/api/v1',
-  lookupEndpoint: 'http://185.218.125.78:8080/invoicemaster/api/v1/data'
+  endpoint: 'https://185.218.125.78:8080/invoicemaster/api/v1',
+  lookupEndpoint: 'https://185.218.125.78:8080/invoicemaster/api/v1/data'
 };
